dfb897a2cf074f3dbe047bbc805fca577a719208
totallynobuzzismaloy <buzzispro@gmail.com>
2024-10-25 00:42:49 +0300

    Updated java main, quiz activities and activity_quiz.xml, manifest is updated too

--- seventh-lab/app/src/main/java/com/example/kendoquizv1337/MainActivity.java
+++ seventh-lab/app/src/main/java/com/example/kendoquizv1337/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.kendoquizv1337;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
@@ -25,5 +27,19 @@ public class MainActivity extends AppCompatActivity {
         Button btnStart = findViewById(R.id.btnStart);
         Button btnRules = findViewById(R.id.btnRules);
         Button btnStatistics = findViewById(R.id.btnStatistics);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, QuizActivity.class));
+            }
+        });
+
+        btnRules.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, RulesActivity.class));
+            }
+        });
     }
 }
--- seventh-lab/app/src/main/java/com/example/kendoquizv1337/QuizActivity.java
+++ seventh-lab/app/src/main/java/com/example/kendoquizv1337/QuizActivity.java
@@ -1,4 +1,29 @@
 package com.example.kendoquizv1337;
 
-public class QuizActivity {
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+public class QuizActivity extends AppCompatActivity {
+    private static final String PREFS_NAME = "QuizStats";
+    private static final String CORRECT_KEY = "correctAnswers";
+    private static final String INCORRECT_KEY = "incorrectAnswers";
+    private static final String TESTS_KEY = "completedTests";
+
+    private ViewPager2 viewPager;
+    private SharedPreferences sharedPreferences;
+
+    int totalCorrectAns, totalWrongAns, totalCompletedTests;
+
+    @Override
+    protected void onCreate(Bundle SavedInstanceState){
+        super.onCreate(SavedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+        ImageView goBack = findViewById(R.id.imageArrow1);
+        goBack.setOnClickListener(v -> finish());
+    }
 }
