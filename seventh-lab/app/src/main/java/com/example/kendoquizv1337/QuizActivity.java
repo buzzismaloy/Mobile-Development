d1504326e8b77dc0e17f31295b4a1a7914a1be1f
totallynobuzzismaloy <buzzispro@gmail.com>
2024-10-26 14:23:28 +0300

    Minor changes in quizActivity(added new elem in Fragment array and proccessing for questions with edit text) and fragment_radio_question(added default checked button)

--- seventh-lab/app/src/main/java/com/example/kendoquizv1337/QuizActivity.java
+++ seventh-lab/app/src/main/java/com/example/kendoquizv1337/QuizActivity.java
@@ -3,8 +3,11 @@ package com.example.kendoquizv1337;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -12,6 +15,7 @@ import androidx.viewpager2.widget.ViewPager2;
 
 public class QuizActivity extends AppCompatActivity {
     private final QuestionAnswers questionAnswers = new QuestionAnswers();
+    private TextView counterTextView;
 
     /*private static final String PREFS_NAME = "QuizStats";
     private static final String CORRECT_KEY = "correctAnswers";
@@ -27,7 +31,8 @@ public class QuizActivity extends AppCompatActivity {
     private final Fragment[] questionFragments = {
             new RadioQuestionFragment(),
             new CheckBoxQuestionFragment(),
-            new EditTextQuestionFragment()
+            new EditTextQuestionFragment(),
+            new PictureQuestionFragment()
     };
 
     @Override
@@ -49,6 +54,7 @@ public class QuizActivity extends AppCompatActivity {
         totalWrongAns = sharedPreferences.getInt(INCORRECT_KEY, 0);
         totalCompletedTests = sharedPreferences.getInt(TESTS_KEY, 0);*/
 
+        counterTextView = findViewById(R.id.counterTextView);
         ImageView goBack = findViewById(R.id.imageArrow1);
         goBack.setOnClickListener(v -> onBackPressed());
     }
@@ -72,15 +78,55 @@ public class QuizActivity extends AppCompatActivity {
     }
 
     public void checkRadioGroup(RadioGroup radioGroup){
+        int selectedId = radioGroup.getCheckedRadioButtonId();
 
+        if (selectedId == R.id.option2) {
+            curCorrectAns++;
+            Toast.makeText(QuizActivity.this, "Correct!", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            curWrongAns++;
+            Toast.makeText(QuizActivity.this, "Incorrect!", Toast.LENGTH_SHORT).show();
+        }
+        updateCounterView();
     }
 
     public void checkCheckBox(View view){
+        CheckBox checkBox1 = view.findViewById(R.id.checkbox1);
+        CheckBox checkBox2 = view.findViewById(R.id.checkbox2);
+        CheckBox checkBox3 = view.findViewById(R.id.checkbox3);
+        CheckBox checkBox4 = view.findViewById(R.id.checkbox4);
 
+        boolean isCheckBox1Checked = checkBox1.isChecked();
+        boolean isCheckBox2Checked = checkBox2.isChecked();
+        boolean isCheckBox3Checked = checkBox3.isChecked();
+        boolean isCheckBox4Checked = checkBox4.isChecked();
+
+        if (isCheckBox1Checked && isCheckBox3Checked && !isCheckBox2Checked && !isCheckBox4Checked) {
+            curCorrectAns++;
+            Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show();
+        } else {
+            curWrongAns++;
+            Toast.makeText(this, "Incorrect", Toast.LENGTH_SHORT).show();
+        }
+        updateCounterView();
     }
 
     public void checkEditTextAnswer(String answer){
+        if (answer.equals(questionAnswers.getAnswer(questionAnswers.getQuestion(questionIndex)).toLowerCase())) {
+            curCorrectAns++;
+            Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            curWrongAns++;
+            Toast.makeText(this, "Incorrect", Toast.LENGTH_SHORT).show();
+        }
+        updateCounterView();
+    }
 
+    private void updateCounterView(){
+        String msg = "Correct: " + curCorrectAns + "  Incorrect: " + curWrongAns;
+        counterTextView.setText(msg);
     }
 
     @Override
